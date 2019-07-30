8bcdf26fe695e109cf6e5d507c8d2c897c6aa3ec
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-07-30 10:18:41 +0530

    Add new elasti and new vpc

---
+++ Retest647/lambda.js
@@ -0,0 +1,27 @@
+let SL_REDIS = require('slappforge-sdk-redis');
+let clusterManager = require('./ClusterManager');
+const redis = new SL_REDIS.Redis(clusterManager);
+
+exports.handler = function (event, context, callback) {
+    // You must always quit the redis client after it's used
+    redis.expire({
+        clusterIdentifier: 'kcluster',
+        params: [{
+            key: 'ID',
+            seconds: 60
+        }, {
+            key: 'Name',
+            seconds: 120
+        }]
+    }, function (error, response, redisClient) {
+        if (error) {
+            callback(error);
+            console.log(error);
+        } else {
+            console.log(response);
+            redisClient.quit();
+        }
+    });
+
+    callback(null, { "message": "Successfully executed" });
+}
